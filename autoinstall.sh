c0c922102564183244d15c824f21f3e9c30c09dd
Kuanlin <kuan.lyn.chen@gmail.com>
2024-03-31 18:35:34 +0800

    2

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install lsb-release gnupg2 apt-transport-https ca-certificates c
 echo "CURL"
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/debian.gpg;
 echo "ADD Repo"
-sudo add-apt-repository –yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
+sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
 echo "UPDATE_2"
 sudo apt-get update;
 sudo apt-get upgrade -y;
@@ -17,7 +17,7 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin;
 sudo apt-cache policy docker-cel;
 sudo apt install -y docker-ce;
 echo "DOCKER ENV"
-sudo systemctl status docker;
+#sudo systemctl status docker;
 sudo groupadd docker;
 sudo gpasswd -a $USER docker;
 echo "POSTGRESQL CLIENT"
@@ -25,8 +25,5 @@ sudo apt-install postgresql-client;
 echo "PYTHON VENV"
 sudo apt install -y python3-venv;
 echo "PYTHON CREATE VENV"
-cd ~;
 mkdir bot;
-cd ~/bot;
-python3 -m venv .venv;
-source .venv/bin/activate;
+python3 -m venv ~/bot/.venv;

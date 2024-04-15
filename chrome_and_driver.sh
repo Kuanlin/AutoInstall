e8101b74b3fcbbff3cf2be12f98a1f5a88b31b14
Kuanlin <kuan.lyn.chen@gmail.com>
2024-04-15 09:33:12 +0800

    Create chrome_and_driver.sh

---
+++ chrome_and_driver.sh
@@ -0,0 +1,4 @@
+wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chromedriver-linux64.zip
+wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chrome-headless-shell-linux64.zip
+unzip chromedriver-linux64.zip;
+unzip chrome-headless-shell-linux64.zip;

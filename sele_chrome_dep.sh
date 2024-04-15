3be78daea4391fb64e4197beb1f0f2c219678dd1
Kuanlin <kuan.lyn.chen@gmail.com>
2024-04-15 09:58:43 +0800

    Update sele_chrome_dep.sh

--- sele_chrome_dep.sh
+++ sele_chrome_dep.sh
@@ -1,3 +1,2 @@
-sudo apt install -y unzip;
 sudo apt install -y libnss3 libxcb1;
 sudo apt install -y libatk1.0-0 libatk-bridge2.0-0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libxkbcommon0 libasound2 libcups2 libpango-1.0-0 libcairo2;
